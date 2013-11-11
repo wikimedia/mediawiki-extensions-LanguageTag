3d05ed9572c85a39a1c4991ead5796a8a6d4ed7f
raymond <raimond.spekking@gmail.com>
2013-11-11 20:50:22 +0100

    Add i18n file
    Change-Id: Ied356c31e50e3863b6d659a6afe079c640c3d3d3

---
+++ LanguageTag.i18n.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Internationalization file for the LanguageTag extension.
+ *
+ * @file
+ * @ingroup Extensions
+ * @author István Király <LaKing@D250.hu>
+ */
+
+$messages = array();
+
+/** English
+ * @author István Király
+ */
+$messages['en'] = array(
+	'languagetag-desc' => 'Allows to use XML-style language tags to markup different languages, and display them properly',
+);
+
+/** Message documentation (Message documentation)
+ * @author Shirayuki
+ */
+$messages['qqq'] = array(
+	'languagetag-desc' => '{{desc|name=Language Tag|url=http://www.mediawiki.org/wiki/Extension:LanguageTag}}',
+);
+
--- LanguageTag.php
+++ LanguageTag.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /**
  * LanguageTag extension from D250 Laboratories
  *
@@ -7,24 +7,23 @@
  * @ingroup Extensions
  * @author István Király <LaKing@D250.hu>
  */
- 
+
 // Yes, this is an extension, not a standalone script, we are coming from LocalSettings.php
 if( !defined( 'MEDIAWIKI' ) ) {
         echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
         die( -1 );
 }
- 
+
 // Standard crediting 
-$wgExtensionCredits['validextensionclass'][] = array(
+$wgExtensionCredits['parserhook'][] = array(
         'path'           => __FILE__,
         'name'           => 'LanguageTag',
         'version'        => '1.0', // 2013.10.28
-        'author'         => 'István Király', 
+        'author'         => 'István Király',
         'url'            => 'https://www.mediawiki.org/wiki/Extension:LanguageTag',
-//      'descriptionmsg' => 'Language Tag extension', // Message key in i18n file.
-        'description'    => 'This extension allows to use XML-style language tags to markup different languages, and display them properly.'
+	'descriptionmsg' => 'languagetag-desc',
 );
- 
- 
+
+$wgExtensionMessagesFiles['LanguageTag'] = __DIR__ . '/LanguageTag.i18n.php';
 $wgAutoloadClasses['LanguageTag'] = __DIR__ . '/LanguageTag.body.php';
 $wgHooks['ParserFirstCallInit'][] = 'LanguageTag::LanguageTagParserInit';
